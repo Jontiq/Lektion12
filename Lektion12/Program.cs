465cd3c9915acea604999d4cf29b42b35a821c14
Jontiq <99jonathancar@gmail.com>
2025-09-22 19:01:56 +0200

    Added the OUT - assignment to the repository

--- Lektion12/Program.cs
+++ Lektion12/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Lektion12
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lektion12
 {
     internal class Program
     {
@@ -9,11 +12,31 @@
             //user.ChangePassword("password123", "123password");
             //user.Login("Jontiq", "123password");
 
-            Product product = new Product("Milk", 15.95m, 25);
-            product.DisplayProduct();
-            product.Restock(50);
-            product.Purchase(20);
-            product.Purchase(31);
+            //Product product = new Product("Milk", 15.95m, 25);
+            //product.DisplayProduct();
+            //product.Restock(50);
+            //product.Purchase(20);
+            //product.Purchase(31);
+
+            // Listan som håller talen
+            List<int> lista = new List<int> { 1, 2, 3, 4, 5, 6, 7, };
+            double average;
+            int sum;
+            //Anropar metod
+            CalculateSumAndAverage(lista, out average, out sum);
+
+            // Skriv ut resultatet
+            Console.WriteLine($"Summan av talen är: {sum}");
+            Console.WriteLine($"Medelvärdet av talen är: {average}");
+        }
+        public static void CalculateSumAndAverage(List <int> list, out double average, out int sum)
+        {
+            sum = 0;
+            foreach(int num in list)
+            {
+                sum += num;
+            }
+            average = sum / list.Count();
         }
     }
 }
